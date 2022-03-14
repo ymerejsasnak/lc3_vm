b643eca8a7ef24c14e23841e9ada068d3806d7dc
ymerejsasnak <ymerejsasnak@gmail.com>
2022-03-14 14:07:03 -0400

    load program

--- lc3_vm.c
+++ lc3_vm.c
@@ -129,11 +129,43 @@ void update_flags(uint16_t r)
 
 
 
+// read lc-3 program into memory
+void read_image_file(FILE* file)
+{
+    uint16_t origin; // origin is where in memory to place the image
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin); // lc3 programs are big endian 
+
+    uint16_t max_read = UINT16_MAX - origin; // biggest size program can possibly be given that total mem is uint16_max
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    // go back through read data, swapping bytes to little endian
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+
+// swap endianness of 16bit number
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
 
 
 
-
-
+// read image file given string path
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; }
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
 
 
 
